d8554182dd66624e3dee0bb8ef98bc8ac31eca55
MinhNamNguyen <44270419+MinhNamNguyen@users.noreply.github.com>
2018-12-17 12:05:54 +0100

    Add files via upload

---
+++ Domination/src/game/Play.java
@@ -0,0 +1,40 @@
+package game;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Play extends BasicGameState{
+	
+	public Play(int state){
+		
+	}
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
---
+++ Domination/src/game/Window.java
@@ -0,0 +1,53 @@
+package game;
+import java.io.File;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class Window extends StateBasedGame{
+
+	public static boolean _APPLET = true ;
+	public static final int menu =0;
+	public static final int play =1;
+	
+	public Window() {
+		super("Domination");
+		this.addState(new Domination(menu));
+		this.addState(new Play(play));
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		_APPLET = false;
+		File f = new File("natives");
+		if(f.exists()) System.setProperty("org.lwljgl.librarypath", f.getAbsolutePath());
+		
+		
+		try {
+			AppGameContainer game = new AppGameContainer(new Window());
+			game.setDisplayMode(windows.WIDTH, windows.HEIGHT, true/*plein écran*/);
+			game.setAlwaysRender(true);
+			game.start();
+			
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		this.getState(menu).init(gc,this);
+		this.getState(play).init(gc,this);
+		this.enterState(menu);
+		gc.setShowFPS(true);
+	}
+
+}
